24cf1109da8349a7945b4ce3dc91e906d7d8b598
Jairo Castaneda <jairo.castaneda@uptc.edu.co>
2022-03-21 17:45:01 -0500

    Back-end -> Inyección de dependencias y creación de modelo

---
+++ Back-end/Models/TarjetaCredito.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Back_end.Models
+{
+    public class TarjetaCredito
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Titular { get; set; }
+        [Required]
+        public string NumeroTarjeta { get; set; }
+        [Required]
+        public string FechaExpiracion { get; set; }
+        [Required]
+        public string CW { get; set; }
+    }
+}
