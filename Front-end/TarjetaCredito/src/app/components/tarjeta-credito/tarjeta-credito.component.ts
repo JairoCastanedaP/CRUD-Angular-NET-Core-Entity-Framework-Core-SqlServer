82d67dd76f4ba24f5609d202add49d5346359c26
Jairo Castaneda <jairo.castaneda@uptc.edu.co>
2022-03-25 13:50:36 -0500

    Front-end - Agregar las Tarjetas

--- Front-end/TarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ Front-end/TarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -12,9 +13,34 @@ export class TarjetaCreditoComponent implements OnInit {
     {titular:'sebastian ferrer',numeroTarjeta:'6854',fechaExpiracion:'2022-01',cvv:'123'},
     {titular:'sofia castaneda',numeroTarjeta:'3215',fechaExpiracion:'2022-02',cvv:'123'}
   ];
-  constructor() { }
+  
+  form:FormGroup;
+  constructor(private fb: FormBuilder) { 
+
+    this.form=fb.group({
+      titular:[''],
+      numeroTarjeta: [''],
+      fechaExpiracion: [''],
+      cvv: ['']
+
+    })
+
+  }
 
   ngOnInit(): void {
+  }
+  agregarTarjeta(){
+    console.log(this.form);
+    
+    const tarjeta: any={
+      titular: this.form.get('titular')?.value,
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+      fechaExpiracion: this.form.get('fechaExpiracion')?.value,
+      cvv: this.form.get('cvv')?.value
+    }
+    console.log(tarjeta);
+    this.listTarjetas.push(tarjeta)
+    this.form.reset()
   }
 
 }
