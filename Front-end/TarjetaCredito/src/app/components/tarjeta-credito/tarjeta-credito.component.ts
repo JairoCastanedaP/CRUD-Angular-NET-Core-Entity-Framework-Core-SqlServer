eeee25881703fb36d710e9eb241aeb3adc827c2c
Jairo Castaneda <jairo.castaneda@uptc.edu.co>
2022-03-24 23:59:15 -0500

    Front-end- Listado de Tarjetas

--- Front-end/TarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ Front-end/TarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TarjetaCreditoComponent implements OnInit {
 
+  listTarjetas: any[]=[
+    {titular:'juaco perez',numeroTarjeta:'6165',fechaExpiracion:'2023-03',cvv:'123'},
+    {titular:'sebastian ferrer',numeroTarjeta:'6854',fechaExpiracion:'2022-01',cvv:'123'},
+    {titular:'sofia castaneda',numeroTarjeta:'3215',fechaExpiracion:'2022-02',cvv:'123'}
+  ];
   constructor() { }
 
   ngOnInit(): void {
