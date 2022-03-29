bcbd36dc40bfc5d5342b6f0b196f04803ea13f20
Jairo Castaneda <jairo.castaneda@uptc.edu.co>
2022-03-29 17:14:08 -0500

    Front-end - Se agregan validaciones al formulario de Tarjetas

--- Front-end/TarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ Front-end/TarjetaCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -18,10 +18,10 @@ export class TarjetaCreditoComponent implements OnInit {
   constructor(private fb: FormBuilder) { 
 
     this.form=fb.group({
-      titular:[''],
-      numeroTarjeta: [''],
-      fechaExpiracion: [''],
-      cvv: ['']
+      titular:['',Validators.required],
+      numeroTarjeta: ['',[Validators.required,Validators.minLength(16),Validators.maxLength(16)]],
+      fechaExpiracion: ['',[Validators.required,Validators.maxLength(5),Validators.minLength(5)]],
+      cvv: ['',[Validators.required,Validators.maxLength(3),Validators.minLength(3)]]
 
     })
 
